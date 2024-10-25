608c4285d858d3a472cc92236af98b097d479820
j.garriguet <j.garriguet@c3rb.fr>
2024-10-25 14:45:17 +0200

    feat: Ajout premier Kata StringsEnds

---
+++ strings_ends.py
@@ -0,0 +1,5 @@
+
+
+def solution(text, ending):
+    # your code here...
+    pass
---
+++ test_strings_ends.py
@@ -0,0 +1,34 @@
+from strings_ends import solution
+import unittest
+
+fixed_tests_True = (
+    ( "samurai", "ai"    ),
+    ( "ninja",   "ja"    ),
+    ( "sensei",  "i"     ),
+    ( "abc",     "abc"   ),
+    ( "abcabc",  "bc"    ),
+    ( "fails",   "ails"  ),
+)
+
+fixed_tests_False = (
+    ( "sumo",    "omo"   ),
+    ( "samurai", "ra"    ),
+    ( "abc",     "abcd"  ),
+    ( "ails",    "fails" ),
+    ( "this",    "fails" ),
+    ( "spam",    "eggs"  )
+)
+
+class TestStringsEnds(unittest.TestCase):
+    
+    print("True Cases")
+    def test_true_case(self):
+        for text, ending in fixed_tests_True:
+            self.assertEqual(solution(text, ending), True)
+    print("False Cases")
+    def test_false_case(self):
+        for text, ending in fixed_tests_False:
+            self.assertEqual(solution(text, ending), False)
+
+if __name__ == '__main__':
+    unittest.main()
