69bfdeb4868e8d98ad0e9946ec037abbd6f48948
j.garriguet <j.garriguet@c3rb.fr>
2024-10-30 10:58:31 +0100

    fix: Ajout autre solution pour le binaire en int

--- binaire_to_int.py
+++ binaire_to_int.py
@@ -8,3 +8,9 @@ def binary_array_to_number(arr):
 
 def binary_array_to_number_best(arr):
   return int("".join(map(str, arr)), 2)
+
+def bin2int(arr):
+  result = 0
+  for i in arr:
+      result = (result * 2) + i
+  return result
--- test_binaire_to_int.py
+++ test_binaire_to_int.py
@@ -1,14 +1,14 @@
-from binaire_to_int import binary_array_to_number
+from binaire_to_int import bin2int
 import unittest
 
 
 class TestStringsEnds(unittest.TestCase):
 
     def test_basic_test_cases(self):
-        self.assertEqual(binary_array_to_number([0,0,0,1]), 1)
-        self.assertEqual(binary_array_to_number([0,0,1,0]), 2)
-        self.assertEqual(binary_array_to_number([1,1,1,1]), 15)
-        self.assertEqual(binary_array_to_number([0,1,1,0]), 6)
+        self.assertEqual(bin2int([0,0,0,1]), 1)
+        self.assertEqual(bin2int([0,0,1,0]), 2)
+        self.assertEqual(bin2int([1,1,1,1]), 15)
+        self.assertEqual(bin2int([0,1,1,0]), 6)
 
 if __name__ == '__main__':
     unittest.main()
